d1dc6e92f26ddcf8db659af1b1d9b588c4a858c9
lazyKT <kyaw.thitlwin.me@gmail.com>
2023-08-14 23:03:22 +0800

    Allow websocket to access dynamodb and return queue_item status to client

--- ws_handler.py
+++ ws_handler.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import boto3
+from utils import dynamodb_to_dict
 
 
 def connection_manager (event, context):
@@ -28,17 +29,39 @@ def default_handler (event, context):
     }
 
 
-def send_message (event, context):
-    print(f"Message sent on web-socker. {event}")
+def check_queue_status (message_id: str):
+    dynamodb = boto3.client('dynamodb')
+    res = dynamodb.get_item(
+        TableName='queueStatusTable',
+        Key={
+            'messageId': { 'S' : message_id }
+        }
+    )
+    print(f'res, {res}')
+    if res['Item'] is not None:
+        return dynamodb_to_dict(res['Item'])
+    else:
+        raise RuntimeError(f'Queue item not found with message id, {message_id}')
+
+def reply_to_client (connection_id: str, data: dict, event: any):
     endpoint_url = 'https://'+event['requestContext']['domainName']+'/'+ event['requestContext']['stage']
+    print(f'[DEBUG] endpoint_url: {endpoint_url}')
     apigateway = boto3.client(
         'apigatewaymanagementapi',
         endpoint_url=endpoint_url
     )
     apigateway.post_to_connection(
-        ConnectionId=event['requestContext']['connectionId'],
-        Data=json.dumps({'message': 'Reply from socket!'}).encode('utf-8')
+        ConnectionId=connection_id,
+        Data=json.dumps({'message': data}).encode('utf-8')
     )
+
+
+def send_message (event, context):
+    print(f"Message sent on web-socker. {event}")
+    connection_id = event['requestContext']['connectionId']
+    message_id = json.loads(event.get('body'))['message']
+    queue_status = check_queue_status(message_id)
+    reply_to_client(connection_id, queue_status, event)
     body = json.loads(event.get("body", ""))
     return {
         "statusCode": 200,
